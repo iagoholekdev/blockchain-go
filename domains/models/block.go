5b004478ec5c5e4525f1a734013ef51d0590b9fe
iagoholek <augustinhocarrara43@gmail.com>
2024-10-29 22:50:44 -0300

    main

--- domains/models/block.go
+++ domains/models/block.go
@@ -1,6 +1,11 @@
 package models
 
-import "github.com/iagoholekdev/blockchain-go/domains/types"
+import (
+	"encoding/binary"
+	"io"
+
+	"github.com/iagoholekdev/blockchain-go/domains/types"
+)
 
 type Header struct {
 	Version   uint32
@@ -8,6 +13,40 @@ type Header struct {
 	TimeStamp uint64
 	Height    uint32
 	Nonce     uint64
+}
+
+func (h *Header) EncodeBinary(w io.Writer) error {
+	if err := binary.Write(w, binary.LittleEndian, &h.Version); err != nil {
+		return err
+	}
+	if err := binary.Write(w, binary.LittleEndian, &h.PrevBlock); err != nil {
+		return err
+	}
+	if err := binary.Write(w, binary.LittleEndian, &h.TimeStamp); err != nil {
+		return err
+	}
+	if err := binary.Write(w, binary.LittleEndian, &h.Height); err != nil {
+		return err
+	}
+
+	return binary.Write(w, binary.LittleEndian, &h.Nonce)
+}
+
+func (h *Header) DecodeBinary(r io.Reader) error {
+	if err := binary.Read(r, binary.LittleEndian, &h.Version); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &h.PrevBlock); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &h.TimeStamp); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &h.Height); err != nil {
+		return err
+	}
+
+	return binary.Read(r, binary.LittleEndian, &h.Nonce)
 }
 
 type Block struct {
